a35d7a0d6f1b50208dd6cbfcf53f06e37484d74b
muha <1student.muhammed1@gmail.com>
2024-05-26 14:11:29 +0300

    feat: render leaderboard with users info

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,25 @@
 import { useNavigate } from 'react-router-dom'
 import waldo from '../assets/waldo.png'
+import { useEffect, useState } from 'react';
 export default function HomePage() {
     const navigate = useNavigate();
 
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        async function fetchUsers() {
+            const response = await fetch('http://localhost:3000/users');
+
+            if (response.ok) {
+                const data = await response.json()
+                setUsers(data.users)
+            }
+
+        }
+
+        fetchUsers()
+    }, [])
+
+    console.log(users)
   return (
       <section className='flex flex-col items-center justify-center min-h-screen bg-red-200'>
           {/* image section */}
@@ -17,7 +34,44 @@ export default function HomePage() {
               <button onClick={() => navigate('/where-is-waldo')} className='bg-orange-400 px-5 py-2 rounded-md font-montserrat'>
                   Find Waldo!
             </button>
+      </div>
+      
+
+
+
+      <div className="relative overflow-x-auto min-w-[32rem]  mt-3">
+    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" className="px-6 py-3">
+                    Name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Duration
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+                users && (
+                    users.map((user) => (
+                        <tr key={user.name} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {user.name}
+                        </th>
+                        <td className="px-6 py-4">
+                            {user.duration / 100}s
+                        </td>
+        
+                    </tr>
+                    ))
+                )
+            }
+      
+        </tbody>
+          </table>
           </div>
+
     </section>
   )
 }
